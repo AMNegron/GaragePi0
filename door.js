3db3fc957e81eb3fc67aa1b7ebd58fc100445a53
AMNegron <AMNegron@users.noreply.github.com>
2017-01-21 21:46:36 -0500

    Formatting

--- door.js
+++ door.js
@@ -1,40 +1,37 @@
-
-
-
-<script>
-    webiopi().ready(function() {
-        	var content, content2, button, content3, button3;
-		content  = $("#content");
-		content2 = $("#openCloseButton"); $(checkStatus);
-		content3 = $("#optionsBtn");
+	webiopi().ready(function() {
+        	var mainbtn, sensorbtn, optbtn, button, button3;
+		mainbtn  = $("#content");
+		sensorbtn = $("#openCloseButton"); $(checkStatus);
+		optbtn = $("#optionsBtn");
 
 	// GPIO 7 = Relay
-	// GPIO17 = Garage Light
-	// GPIO18 = Door Sensor
+	// GPIO17 = Light
+	// GPIO18 = Sensor#1
 	    webiopi().setFunction(7,"in");
 	    webiopi().setFunction(17,"out");
 	    webiopi().setFunction(18,"in");
 
     // create an "OPEN" labeled button for GPIO 18
         button = webiopi().createGPIOButton(18, "");
-        content2.append(button); // append button to content div
+        sensorbtn.append(button); // append button to sensorbtn div
 
-    // create main garage button that calls the ButtonPrsd function below
-        button = webiopi().createButton("button", "GARAGE DOOR", showConfirm );
-        content.append(button); // append button to content div
+    // create main door button that calls the ButtonPrsd function below
+        button = webiopi().createButton("button", "MAIN DOOR", showConfirm );
+        mainbtn.append(button); // append button to mainbtn div
 
     // create options button that calls the showOptions function below
-        button3 = webiopi().createButton("options", "OPTIONS", showOptions );
-        content3.append(button3); // append button to content div
+        button3 = webiopi().createButton("button", "OPTIONS", showOptions );
+        optbtn.append(button3); // append button to optbtn div
 
-	// checks status of door sensor	
-	    function checkStatus() {
-		if (	$('#gpio18').hasClass('LOW')) {
+	// checks status of sensor1, after checking wait 1 sec (1000 milliseconds)	
+	    function checkStatus1() {
+			if (	$('#gpio18').hasClass('LOW')) {
 		    	$("#gpio18").html('CLOSED');
-			}else {
+				}
+			else {
 		    	$("#gpio18").html('OPEN');
-			}
-		setTimeout(checkStatus,1000)
+				}
+			setTimeout(checkStatus1,1000)
 		}
 
 	// gets called by the ButtonPrsd function below - sets gpio7 back to 'IN'
@@ -48,32 +45,42 @@
             document.getElementById('confirmText').style.visibility = "hidden";
         }
 
-	// sets gpio7 to 'OUT' which will trip relay.
-	// After .5 second, calls hideConfirm above,
-	// hides the confirmation div and changes door image
+	// change normal-image to action
+		function ImgAction() {
+			document.getElementById('button').style.background = "#808080 url('/door-action.gif') no-repeat center top";
+		}
+		
+	// change action-image to normal
+		function ImgNormal() {
+			document.getElementById('button').style.background = "#808080 url('/door-static.png') no-repeat center bottom";
+		}
+
+	// sets gpio7 to 'OUT' which will trip relay & changes to action image.
+	// After .5 second (500 millisec), calls ButtonRls & hideConfirm above,
+	// which hides the confirmation div. After 12 sec (12000 millisec), 
+	// returns to static image.
 		function ButtonPrsd() {
-            document.getElementById('button').style.background = "#808080 url('/door-action2.gif') no-repeat center top";
 			webiopi().setFunction(7,"out");
-			hideConfirm();
+			ImgAction();
 			setTimeout(ButtonRls, 500);
-    // Set time (in ms) to display action image ex: 12000
+			hideConfirm();
 			setTimeout(ImgNormal, 12000); 
 		}
 
-	// show confirmation div after pressing garage door button
+	// show confirmation div after pressing mainbtn
 		function showConfirm() {
 			document.getElementById('yes').onclick = ButtonPrsd;
 			document.getElementById('no').onclick = hideConfirm;
 			document.getElementById('confirmBox').style.visibility = "visible";
 			document.getElementById('confirmText').style.visibility = "visible";
 		}
-				
-	// change action-image to normal
-		function ImgNormal() {
-			document.getElementById('button').style.background = "#808080 url('/door-static3.png') no-repeat center bottom";
-		}
 
-// The following functions are used for the options control display -->
+
+
+
+
+
+// The following functions are used for the options control display.
 
 	// show options div after pressing OPTIONS button
 		function showOptions() {
@@ -104,5 +111,3 @@
 		webiopi().refreshGPIO(true)
 		}
 	);
-
-    </script>
